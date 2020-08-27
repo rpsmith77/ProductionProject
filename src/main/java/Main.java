af8291e890f9e7e85a412b0418e28f3ae74ed22f
Rpsmith77 <rpsmith77@gmail.com>
2020-08-26 20:33:29 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+    Scene scene = new Scene(root, 300, 275);
+
+    primaryStage.setTitle("FXML Welcome");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+}
+
