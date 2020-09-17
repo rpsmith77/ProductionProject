5b98606bb70cbe2e0dca1307b125a22ca4fd0c78
Rpsmith77 <rpsmith77@gmail.com>
2020-09-16 21:56:39 -0400

    Added limited functionality to Products tab

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -13,10 +13,11 @@ import javafx.scene.control.TextField;
 public class Controller {
 
   @FXML
-  public ChoiceBox<String> cmbItemType;
+  public ChoiceBox<String> choiceItemType;
 
   @FXML
   public ComboBox<String> cmbQuantity;
+
 
   @FXML
   private TextField txtProductName;
@@ -32,18 +33,65 @@ public class Controller {
 
 
   public void addProduct(ActionEvent actionEvent) {
-    System.out.println("You pressed 'Add Product'");
+    addProductDb();
   }
 
   public void recordProduction(ActionEvent actionEvent) {
     System.out.println("You pressed 'Record Product'");
   }
 
-  public void initialize(){
+  public void initialize() {
     connectDb();
+    for (int i = 1; i <= 10; i++) {
+      cmbQuantity.getItems().add(String.valueOf(i));
+    }
+    cmbQuantity.setEditable(true);
+    cmbQuantity.getSelectionModel().selectFirst();
   }
 
   public void connectDb() {
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./res/ProdDB";
+
+    //  Database credentials
+    final String USER = "";
+    final String PASS = "";
+    Connection conn = null;
+    Statement stmt = null;
+
+    //choiceItemType options
+    String[] choiceItemTypeOptions = new String[1];
+    choiceItemTypeOptions[0] = "AUDIO";
+
+    try {
+      // STEP 1: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
+
+      //STEP 2: Open a connection
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+      //STEP 3: Execute a query
+      stmt = conn.createStatement();
+
+      for (int i = 0; i < choiceItemTypeOptions.length; i++) {
+        choiceItemType.getItems().add(choiceItemTypeOptions[i]);
+      }
+
+//      String sql = "INSERT INTO Product(type, manufacturer, name) "
+//          + "VALUES ( 'AUDIO', 'Apple', 'iPod' )";
+//
+//      stmt.executeUpdate(sql);
+
+      // STEP 4: Clean-up environment
+      stmt.close();
+      conn.close();
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
+
+    }
+  }
+
+  public void addProductDb() {
     final String JDBC_DRIVER = "org.h2.Driver";
     final String DB_URL = "jdbc:h2:./res/ProdDB";
 
@@ -63,21 +111,22 @@ public class Controller {
       //STEP 3: Execute a query
       stmt = conn.createStatement();
 
-      String sql = "SELECT * FROM JOBS";
+      String prodName = txtProductName.getText();
+      String prodManufacturer = txtManufacturer.getText();
+      String prodItemType = choiceItemType.getValue();
 
-      ResultSet rs = stmt.executeQuery(sql);
-      while (rs.next()) {
-        System.out.println(rs.getString(1));
-      }
+      String sql = "INSERT INTO Product(type, manufacturer, name) "
+          + "VALUES ( '" + prodItemType + "','" + prodManufacturer + "', '" + prodName + "' )";
+
+      stmt.executeUpdate(sql);
 
       // STEP 4: Clean-up environment
       stmt.close();
       conn.close();
-    } catch (ClassNotFoundException e) {
+    } catch (ClassNotFoundException | SQLException e) {
       e.printStackTrace();
 
-    } catch (SQLException e) {
-      e.printStackTrace();
     }
+
   }
 }
