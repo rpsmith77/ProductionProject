dc7f4f9fd99550abf626b44b891c1e61f755661e
Ryan Smith <rpsmith77@gmail.com>
2020-11-01 20:02:52 -0500

    commented out testing

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -75,8 +75,6 @@ public class Controller {
         txtAreaProductionLog.setText(txtAreaProductionLog.getText() + "\n" + pr.toString());
       }
     }
-
-
   }
 
   // automatically run
@@ -243,22 +241,22 @@ public class Controller {
   }
 
   // test
-  public static void testMultimedia() {
-    AudioPlayer newAudioProduct = new AudioPlayer("DP-X1A", "Onkyo",
-        "DSD/FLAC/ALAC/WAV/AIFF/MQA/Ogg-Vorbis/MP3/AAC", "M3U/PLS/WPL");
-    Screen newScreen = new Screen("720x480", 40, 22);
-    MoviePlayer newMovieProduct = new MoviePlayer("DBPOWER MK101", "OracleProduction",
-        newScreen,
-        MonitorType.LCD);
-    ArrayList<MultimediaControl> productList = new ArrayList<MultimediaControl>();
-    productList.add(newAudioProduct);
-    productList.add(newMovieProduct);
-    for (MultimediaControl p : productList) {
-      System.out.println(p);
-      p.play();
-      p.stop();
-      p.next();
-      p.previous();
-    }
-  }
+//  public static void testMultimedia() {
+//    AudioPlayer newAudioProduct = new AudioPlayer("DP-X1A", "Onkyo",
+//        "DSD/FLAC/ALAC/WAV/AIFF/MQA/Ogg-Vorbis/MP3/AAC", "M3U/PLS/WPL");
+//    Screen newScreen = new Screen("720x480", 40, 22);
+//    MoviePlayer newMovieProduct = new MoviePlayer("DBPOWER MK101", "OracleProduction",
+//        newScreen,
+//        MonitorType.LCD);
+//    ArrayList<MultimediaControl> productList = new ArrayList<MultimediaControl>();
+//    productList.add(newAudioProduct);
+//    productList.add(newMovieProduct);
+//    for (MultimediaControl p : productList) {
+//      System.out.println(p);
+//      p.play();
+//      p.stop();
+//      p.next();
+//      p.previous();
+//    }
+//  }
 }
